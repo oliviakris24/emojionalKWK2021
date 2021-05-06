b420ec45e9643f9c35053c1bb5da4a160ff169d9
oliviakris24 <okfishy@gmail.com>
2021-05-06 15:32:36 -0700

    iteration2 complete :)

--- emojionalKWK2021/ViewController.swift
+++ emojionalKWK2021/ViewController.swift
@@ -14,13 +14,16 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+     let emojis = ["😁": "happy", "😔": "sad", "🥰": "love"]
+    
     @IBAction func showMessage(sender: UIButton) {
+        let selectedEmotion = sender.titleLabel?.text
+        
         let alertController = UIAlertController(title: "Song Recommendation!", message: "Stay, Stay, Stay seems like a pretty good option!", preferredStyle: UIAlertController.Style.alert)
                
                alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
                
                present(alertController, animated: true, completion: nil)
     }
-
 }
 
